bd96334ca68eb1e88da35b5100196f77a323698e
code-larry <larry.basin@gmail.com>
2021-03-22 14:20:28 +0100

    Add properties catalog and feature section on homepage

--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,19 @@ import styles from '../styles/Home.module.css'
 import {Layout} from "../components/layout"
 import api from '../auth/axios'
 import {PropertyVip} from '../components/propertyVip'
+import {PropertySection} from '../components/propertySection'
 import {MDBContainer} from 'mdbreact'
 import {Carousel} from '../components/carousel'
+import {Features} from '../components/features'
 
-export default function Home({propertiesVip}) {
+export default function Home({propertiesVip, properties}) {
   return (
     <Layout>
 	<Carousel />
 	<MDBContainer>
 		<PropertyVip properties={propertiesVip}/>
+		<PropertySection properties={properties}/>
+		<Features />
 	</MDBContainer>
 	</Layout>
   )
@@ -19,10 +23,13 @@ export default function Home({propertiesVip}) {
 
 export const getStaticProps = async() => {
 	const {data: propertiesVip} = await api.get("/api/properties/vip")
+	const {data} = await api.get("/api/properties?limit=6")
+	const properties = data.data
 
 	return {
 		props: {
-			propertiesVip
+			propertiesVip,
+			properties
 		}
 	}
 }
